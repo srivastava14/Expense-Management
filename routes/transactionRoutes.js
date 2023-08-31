e3ca24c05bbbc8e4744217273c4fa1aff837776a
srivastava14 <avi.kanpur14@gmail.com>
2023-08-31 05:30:57 +0530

    code added

---
+++ routes/transactionRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { addTransaction, getAllTransaction, editTransaction, deleteTransaction } = require("../controllers/transactionController");
+
+
+//router object
+const router = express.Router();
+
+//routes
+//add transaaction POST
+router.post('/add-transaction', addTransaction)
+
+//edit transaction POST
+router.post('/edit-transaction', editTransaction)
+
+//delete transaction POST
+router.post('/delete-transaction', deleteTransaction)
+
+//get
+router.post('/get-transaction', getAllTransaction)
+
+module.exports = router;
